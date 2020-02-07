d0f7cfbe7321a0f34418078b03541229f89a74a6
Danny Moulton <ddmoulton@gmail.com>
2020-02-06 22:10:47 -0500

    added license files

--- core/src/main/java/io/kestros/commons/uilibraries/config/UiLibraryConfigurationFactory.java
+++ core/src/main/java/io/kestros/commons/uilibraries/config/UiLibraryConfigurationFactory.java
@@ -1,6 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.kestros.commons.uilibraries.config;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
@@ -38,14 +56,11 @@ public class UiLibraryConfigurationFactory implements Serializable {
     @AttributeDefinition(name = "Minified UI Library Paths",
                          description = "Path to UI Libraries to be "
                                        + "automatically minified. Requests without the `.min` "
-                                       + "selector will be minified,")
-    String[] minifiedLibraryPaths() default {};
+                                       + "selector will be minified,") String[] minifiedLibraryPaths() default {};
   }
 
   private static final Logger LOG = LoggerFactory.getLogger(UiLibraryConfigurationFactory.class);
 
-  // TODO get away from using this if possible
-  @SuppressFBWarnings("SE_TRANSIENT_FIELD_NOT_RESTORED")
   private transient Config config;
 
   /**
