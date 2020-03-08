e0da33259daa50d05bc977bee7c707241aa5c757
Danny Moulton <danny.moulton@kestros.io>
2020-03-07 21:27:20 -0500

    updated structured sling model annotation names

--- core/src/main/java/io/kestros/commons/uilibraries/filetypes/css/CssFile.java
+++ core/src/main/java/io/kestros/commons/uilibraries/filetypes/css/CssFile.java
@@ -21,7 +21,7 @@ package io.kestros.commons.uilibraries.filetypes.css;
 
 import static io.kestros.commons.uilibraries.filetypes.ScriptType.CSS;
 
-import io.kestros.commons.structuredslingmodels.annotation.StructuredModel;
+import io.kestros.commons.structuredslingmodels.annotation.KestrosModel;
 import io.kestros.commons.structuredslingmodels.filetypes.FileType;
 import io.kestros.commons.uilibraries.filetypes.ScriptFile;
 import org.apache.sling.api.resource.Resource;
@@ -30,7 +30,7 @@ import org.apache.sling.models.annotations.Model;
 /**
  * Model type for CSS files to be adapted to.
  */
-@StructuredModel(validationService = CssFileValidationService.class)
+@KestrosModel(validationService = CssFileValidationService.class)
 @Model(adaptables = Resource.class,
        resourceType = "nt:file")
 public class CssFile extends ScriptFile {
