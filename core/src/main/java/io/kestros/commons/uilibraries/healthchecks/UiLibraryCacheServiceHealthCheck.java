0c97da0e0ee006f31aeb7b241e13c6b964b2abee
Danny Moulton <danny.moulton@kestros.io>
2020-07-21 20:10:57 -0400

    added license head and javadoc

--- core/src/main/java/io/kestros/commons/uilibraries/healthchecks/UiLibraryCacheServiceHealthCheck.java
+++ core/src/main/java/io/kestros/commons/uilibraries/healthchecks/UiLibraryCacheServiceHealthCheck.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.kestros.commons.uilibraries.healthchecks;
 
 import io.kestros.commons.uilibraries.services.cache.UiLibraryCacheService;
@@ -14,6 +33,9 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
+/**
+ * Health Check that looks to see if the UiLibraryCacheService is active and running properly.
+ */
 @Component
 @HealthCheckService(name = "UiLibraryCacheService Check",
                     tags = {"kestros", "ui-libraries"})
@@ -23,11 +45,9 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 @Sticky(keepNonOkResultsStickyForSec = 10)
 public class UiLibraryCacheServiceHealthCheck implements HealthCheck {
 
-
   @Reference(cardinality = ReferenceCardinality.OPTIONAL,
              policyOption = ReferencePolicyOption.GREEDY)
   private UiLibraryCacheService uiLibraryCacheService;
-
 
   @Override
   public Result execute() {
